e317cccd64c181be01df194e8a7910eda40b8617
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-08 18:27:12 -0800

    makes a circle from points and add waves to each points radius

--- src/blob.js
+++ src/blob.js
@@ -13,9 +13,10 @@ class Blobbo {
     fill('#91fc9e')
     beginShape()
     for ( let num = 0; num < this.numOfPoints; num += 1) {
-      let h = this.wave(num, 20, 5) + this.wave(num, 20, 3)
-      let x = 100 + num * 2
-      let y = 100 + h
+      let angle = TWO_PI * num / this.numOfPoints
+      let r = 250 + this.wave(num, 20, 5) + this.wave(num, 20, 3)
+      let x = (windowWidth / 2) + r * cos(angle)
+      let y = (windowHeight / 2) + r * sin(angle)
       vertex(x, y)
     }
     endShape()
