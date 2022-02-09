5e9807c5f01958e3dc7373f66677198d070edab4
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-08 18:03:52 -0800

    adds sin wave

--- src/blob.js
+++ src/blob.js
@@ -7,9 +7,11 @@ class Blobbo {
     noStroke()
     fill('#91fc9e')
     for ( let num = 0; num < this.numOfPoints; num += 1) {
+      let h = 70 * sin(TWO_PI * num / this.numOfPoints)
+
       let x = 100 + num * 2
-      let y = 100
-      circle(x, y, 20)
+      let y = 100 + h
+      circle(x, y, 2)
     }
 
 
