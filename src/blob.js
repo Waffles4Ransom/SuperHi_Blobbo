52a81809df6fa63dc6517b9382dd5e3cba71c46f
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-08 18:09:28 -0800

    add movement to sin wave, refactor

--- src/blob.js
+++ src/blob.js
@@ -1,19 +1,22 @@
 class Blobbo {
   constructor() {
-    this.numOfPoints = 360
+    this.numOfPoints = 500
+    this.time = 0
+  }
+
+  wave(num, amp, freq) {
+    return amp * sin(freq * TWO_PI * num / this.numOfPoints + this.time)
   }
 
   draw() {
     noStroke()
     fill('#91fc9e')
     for ( let num = 0; num < this.numOfPoints; num += 1) {
-      let h = 70 * sin(TWO_PI * num / this.numOfPoints)
-
+      let h = this.wave(num, 70, 5)
       let x = 100 + num * 2
       let y = 100 + h
       circle(x, y, 2)
     }
-
-
+    this.time += 0.01
   }
 }
