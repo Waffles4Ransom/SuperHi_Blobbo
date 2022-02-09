49f334b37b0c6fa2a2dcf327ba9846d890acd490
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-08 18:38:30 -0800

    gives the waves a speed, add additional wave to shape

--- src/blob.js
+++ src/blob.js
@@ -4,8 +4,8 @@ class Blobbo {
     this.time = 0
   }
 
-  wave(num, amp, freq) {
-    return amp * sin(freq * TWO_PI * num / this.numOfPoints + this.time)
+  wave(num, amp, freq, speed) {
+    return amp * sin(freq * TWO_PI * num / this.numOfPoints + this.time * speed)
   }
 
   draw() {
@@ -14,7 +14,7 @@ class Blobbo {
     beginShape()
     for ( let num = 0; num < this.numOfPoints; num += 1) {
       let angle = TWO_PI * num / this.numOfPoints
-      let r = 250 + this.wave(num, 20, 5) + this.wave(num, 20, 3)
+      let r = 250 + this.wave(num, 20, 5, 1) + this.wave(num, 20, 3, 3) + this.wave(num, 5, 9, 0)
       let x = (windowWidth / 2) + r * cos(angle)
       let y = (windowHeight / 2) + r * sin(angle)
       vertex(x, y)
