1ae0dc1f69bf328bc389f2b6e914a1f68835ce5b
Jenna Ransom <jenna.ransom@gmail.com>
2022-02-08 18:16:13 -0800

    add additional wave and begin to craft a custom shape with p5

--- src/blob.js
+++ src/blob.js
@@ -11,12 +11,14 @@ class Blobbo {
   draw() {
     noStroke()
     fill('#91fc9e')
+    beginShape()
     for ( let num = 0; num < this.numOfPoints; num += 1) {
-      let h = this.wave(num, 70, 5)
+      let h = this.wave(num, 20, 5) + this.wave(num, 20, 3)
       let x = 100 + num * 2
       let y = 100 + h
-      circle(x, y, 2)
+      vertex(x, y)
     }
+    endShape()
     this.time += 0.01
   }
 }
